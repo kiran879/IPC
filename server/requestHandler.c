1f32ba397f17f17dcbc75074bca446504190c3f3
kiran879 <rajascricketer@gmail.com>
2024-09-04 06:17:30 +0100

    Added request validations

--- server/requestHandler.c
+++ server/requestHandler.c
@@ -26,28 +26,50 @@ void* requestHandler(void *arg)
  		exit(EXIT_FAILURE);
  	}
  	printf("%c\n",rqst->ch);
-	pid=fork();
-	if(pid==0)
+	if(rqst->ch == '+' || rqst->ch == '-' || rqst->ch == '*' || rqst->ch == '/')
 	{
-		
-		switch(rqst->ch)
+		pid=fork();
+		if(pid==0)
 		{
-			case '+':
-				wret=write(*ipcs->pipes+1,rqst,sizeof(request));
-				if(wret==-1)
-				{
-					perror("write");
-					exit(EXIT_FAILURE);
-				}
-				sprintf(fd,"%d",*ipcs->pipes);
-				ret=execl("../vendors/vendorMain","vendorMain",fd,NULL);
-				if(ret==-1)
-				{
-					perror("execl");
-					exit(EXIT_FAILURE);
-				}
-				break;
+					wret=write(*ipcs->pipes+1,rqst,sizeof(request));
+					if(wret==-1)
+					{
+						perror("write");
+						exit(EXIT_FAILURE);
+					}
+					sprintf(fd,"%d",*ipcs->pipes);
+					ret=execl("../vendors/vendorOperations","vendorOperations",fd,NULL);
+					if(ret==-1)
+					{
+						perror("execl");
+						exit(EXIT_FAILURE);
+					}
+					/*
+		
+			switch(rqst->ch)
+			{
+				case '+':
+					wret=write(*ipcs->pipes+1,rqst,sizeof(request));
+					if(wret==-1)
+					{
+						perror("write");
+						exit(EXIT_FAILURE);
+					}
+					sprintf(fd,"%d",*ipcs->pipes);
+					ret=execl("../vendors/vendorMain","vendorMain",fd,NULL);
+					if(ret==-1)
+					{
+						perror("execl");
+						exit(EXIT_FAILURE);
+					}
+					break;
+			}*/
 		}
+	}
+	else //write down into the message queue for invalid request, as the client is block on read for the result
+	{
+		printf("Invalid Request\n");
+		exit(EXIT_FAILURE);
 	}
 
 #ifdef DEBUG
